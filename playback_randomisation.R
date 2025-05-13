22b5365c1fc0b45e89a98044e615281508be85e2
c-noonan-02 <charlottenoonan@outlook.com>
2025-05-13 18:35:51 +0100

    Updated code with reduced species list, and 4x playback expeirments with 4 different speaker placements.

--- playback_randomisation.R
+++ playback_randomisation.R
@@ -10,40 +10,48 @@ rm(list = ls())
 ##### Phase One: Woodland Site #####
 
 # generate a dataframe, containing row of all playback recordings to be played
-playback_recordings <- c("dunnock", "chiffchaff", "willow_warbler", "wren", "great_tit", "robin", "goldcrest", "chaffinch", "blackbird", "wood_pigeon")
+#playback_recordings <- c("dunnock", "chiffchaff", "willow_warbler", "wren", "great_tit", "robin", "goldcrest", "chaffinch", "blackbird", "wood_pigeon")
+# edited species list, with 5 instead of 10
+playback_recordings <- c("dunnock", "wren", "great_tit", "blackbird", "wood_pigeon")
 
 # generate a dataframe with the (random) playback schedule for each AudioMoth
 # generate empty dataframe for overall schedule
 woodland_playback_schedule <- data.frame(
-  audiomoth_ID = rep(NA, 200),
-  species_call = rep(NA, 200)
+  experiment_no = rep(NA, 400),
+  audiomoth_ID = rep(NA, 400),
+  species_call = rep(NA, 400)
 )
 
 # Add each randomised schedule to woodland_playback_schedule
-for (i in 1:20) {
+for (exp in 1:4) {
   
-  # Loop to create 20 shuffled datasets
-  playback <- data.frame(
-    audiomoth_ID = i,
-    species_call = playback_recordings,
-    stringsAsFactors = FALSE
-  )
+  # create a temporary dataframe to store one experiment's playback schedule
+  temp_schedule <- data.frame()
   
-  # row indexing for adding data to combined dataset
-  start_row <- (i-1) * 10 + 1
-  end_row <- i * 10
+  # create 20 shuffled datasets
+  for (i in 1:20) {
+    
+    playback <- data.frame(
+      experiment_no = exp,
+      audiomoth_ID = i,
+      species_call = sample(playback_recordings),
+      stringsAsFactors = FALSE
+    )
+    
+    # bind to temporary schedule for this experiment repetition
+    temp_schedule <- rbind(temp_schedule, playback)
+  }
   
+  # calculate rows for the data
+  start_row <- (exp-1) * 100 + 1
+  end_row <- exp * 100
+    
   # add the schedule for AudioMoth[i] to the appropriate part of the combined schedule
-  woodland_playback_schedule[start_row:end_row, ] <- playback
-}
-
-# now randomise the schedule, to avoid any pseudoreplication
-woodland_playback_schedule <- woodland_playback_schedule[sample(nrow(woodland_playback_schedule)), ]
+  woodland_playback_schedule[start_row:end_row, ] <- temp_schedule
+  }
 
 # check final schedule
 View(woodland_playback_schedule)
-
-# might need to add additional code if I am going to repeat this 4x at each corner of the grid - but need to discuss time constraints with supervisor
 
 # save the schedule to the project folder as reference for designing playbacks
 write.csv(woodland_playback_schedule, "./data/phase1_woodland_playback.csv", row.names = FALSE)
